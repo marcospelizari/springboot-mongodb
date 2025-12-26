1cd92fc65eaa6ce1d8ae5aa3343dafaf304dbae4
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-26 15:08:06 -0300

    Consulta simples pelo titlo

--- Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/controller/PostController.java
+++ Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/controller/PostController.java
@@ -1,12 +1,12 @@
 package com.mongostudy.workshopmongo.controller;
 
+import com.mongostudy.workshopmongo.controller.util.URL;
 import com.mongostudy.workshopmongo.entities.Post;
 import com.mongostudy.workshopmongo.service.PostService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -22,5 +22,12 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
