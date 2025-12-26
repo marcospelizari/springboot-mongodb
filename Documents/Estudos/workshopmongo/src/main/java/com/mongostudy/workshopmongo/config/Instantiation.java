845aa8f93115ecab970e6e578fd706cae8ed43d1
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-26 14:49:04 -0300

    Comment in posts

--- Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/config/Instantiation.java
+++ Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.mongostudy.workshopmongo.config;
 
 import com.mongostudy.workshopmongo.dto.AuthorDTO;
+import com.mongostudy.workshopmongo.dto.CommentDTO;
 import com.mongostudy.workshopmongo.entities.Post;
 import com.mongostudy.workshopmongo.entities.User;
 import com.mongostudy.workshopmongo.repository.PostRepository;
@@ -33,10 +34,21 @@ public class Instantiation implements CommandLineRunner {
 
         postRepository.deleteAll();
 
-         User maria = userService.findById("694e8b7b08ac7eec18fe1b24");
+        User maria = userService.findById("694e8b7b08ac7eec18fe1b24");
+        User aria = userService.findById("694e9967dc702af0d0458a8c");
+        User erica = userService.findById("694e8bbf08ac7eec18fe1b26");
+        User lucy = userService.findById("694eb5293d32683f4d398fcf");
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!!!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+
+        CommentDTO c1 = new CommentDTO("Boa viagem, gata", sdf.parse("21/03/2018"), new AuthorDTO(aria));
+        CommentDTO c2 = new CommentDTO("Aproveita!!!", sdf.parse("22/03/2018"), new AuthorDTO(erica));
+        CommentDTO c3 = new CommentDTO("Have a nice day, baby.", sdf.parse("23/03/2018"), new AuthorDTO(lucy));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
