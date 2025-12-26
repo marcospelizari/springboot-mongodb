f751b1860ebae9011507fa3a6b6e447f4c0d7ff5
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-26 14:31:27 -0300

    Implementacao do post find by id

---
+++ Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.mongostudy.workshopmongo.controller;
+
+import com.mongostudy.workshopmongo.entities.Post;
+import com.mongostudy.workshopmongo.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = postService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
