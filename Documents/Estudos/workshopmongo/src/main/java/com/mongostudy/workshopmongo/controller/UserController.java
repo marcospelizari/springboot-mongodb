01f05f3f67e5581f39b699b997a8ea9db3d2e99e
Marcos <132465567+marcospelizari@users.noreply.github.com>
2025-12-26 13:23:51 -0300

    Delecao de usuario com DELETE

--- Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/controller/UserController.java
+++ Documents/Estudos/workshopmongo/src/main/java/com/mongostudy/workshopmongo/controller/UserController.java
@@ -42,5 +42,11 @@ public class UserController {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
